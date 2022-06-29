367836fa49847be206a21384d6a4ddff42d471a6
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-06-30 00:35:56 +0530

    Added Solution 1 for Problem 2 in Java

---
+++ Easy_Problems/2. Validate Subsequence/Java/Solution1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Solution1{
+
+    // O(n) time | O(1) space
+    // Pointers with While Loop
+     public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+        int arrIdx = 0;
+        int seqIdx = 0;
+        while (arrIdx < array.size() && seqIdx < sequence.size()) {
+            if (array.get(arrIdx) == sequence.get(seqIdx)) {
+                seqIdx++;
+            }
+            arrIdx++;
+        }
+        return seqIdx == sequence.size();
+    }
+
+
+}
