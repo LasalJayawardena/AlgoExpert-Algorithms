c68f33aeade56bcdf2827ab7905974159b877355
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-06-30 22:58:57 +0530

    Added Solution 1 for Problem 3 in Python

---
+++ Easy_Problems/3. Sorted Square Array/Python/Solution1.py
@@ -0,0 +1,15 @@
+# O(nlog(n)) time | O(n) space
+# Brute Force Approach
+
+def sortSquareArray(array):
+    
+    new_arr = [x**2 for x in array]
+    new_arr.sort()
+    return new_arr
+
+
+# Tests for sortSqaureArray
+assert(sortSquareArray([-4, -1, 0, 3, 10]) == [0, 1, 9, 16, 100])
+assert(sortSquareArray([-7, -3, 2, 3, 11]) == [4, 9, 9, 49, 121])
+assert(sortSquareArray([-2, -1, 0, 2, 10]) == [0, 1, 4, 4, 100])
+assert(sortSquareArray([0, 1, 4, 7, 9, 6]) == [0, 1, 16, 36, 49, 81])
