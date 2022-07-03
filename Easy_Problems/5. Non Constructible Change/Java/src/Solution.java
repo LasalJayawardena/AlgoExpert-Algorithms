eb77a9d1ea1f8884f664d1e35afcbe0212de467f
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-04 00:27:46 +0530

    Added Tests for Solution for Problem 5 in Java

--- Easy_Problems/5. Non Constructible Change/Java/src/Solution.java
+++ Easy_Problems/5. Non Constructible Change/Java/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Solution {
 
     // O(nlog(n)) time | O(1) space O(n) space if we cannot mutate input data structure
@@ -8,7 +10,7 @@ public class Solution {
     // Imagine we have another value called C, representing the change we can create with our coins: C = 1.
     // We have also V which represents the new coin we add to our set. If V > C + 1, we cannot make C + 1 change; if V <= C + 1, we can make from C to C + V change, and the minimum amount of change we cannot create is C + V + 1.
     public static int getNonConstructibleChange(int[] coins) {
-        coins.sort();
+        Arrays.sort(coins);
 
         int currentChangeCreated = 0;
         for(int coin: coins) {
@@ -21,4 +23,11 @@ public class Solution {
 
         return currentChangeCreated + 1;
     }
+
+    public static void main(String[] args) {
+        System.out.println(getNonConstructibleChange(new int[] {5, 7, 1, 1, 2, 3, 22})); // 20
+        System.out.println(getNonConstructibleChange(new int[] {5})); // 1
+        System.out.println(getNonConstructibleChange(new int[] {1, 2, 5})); // 4
+    }
 }
+
