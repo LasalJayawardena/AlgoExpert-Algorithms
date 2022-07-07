f57828d6f080d732d635308fca6b3cc06c32e9a6
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-07 18:01:13 +0530

    Added Tests for Solution2 for Problem 6 in Java

--- Easy_Problems/6. Find Closest Value In BST/Java/src/Solution2.java
+++ Easy_Problems/6. Find Closest Value In BST/Java/src/Solution2.java
@@ -4,10 +4,10 @@ public class Solution2 {
     // Worst: O(n) time | O(1) space
     // Iterative solution
     public static int findClosestValueInBst(BST tree, int target) {
-        return findClosestValueInBst(tree, target, Double.MAX_VALUE);
+        return findClosestValueInBst(tree.root, target, Integer.MAX_VALUE/2);
     }
-    public static int findClosestValueInBst(BST tree, int target, int closest) {
-        Node current = tree.root;
+    public static int findClosestValueInBst(Node tree, int target, int closest) {
+        Node current = tree;
         while (current != null) {
             if (Math.abs(target - closest) > Math.abs(target - current.value)) {
                 closest = current.value;
@@ -21,5 +21,18 @@ public class Solution2 {
             }
         }
         return closest;
+    }
+
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.build(new int[]{10, 4, 1, 6, 20});
+
+        System.out.println(Solution2.findClosestValueInBst(bst, -1)); //1
+        System.out.println(Solution2.findClosestValueInBst(bst, 4)); // 4
+
+        bst = new BST();
+        bst.build(new int[]{1, -2, 5, 40, 5,- 6, 7, 8, 9, 10});
+        System.out.println(Solution2.findClosestValueInBst(bst, 25)); // 40
+        System.out.println(Solution2.findClosestValueInBst(bst, -3)); // -2
     }
 }
