78d739545a8bf5c5557989f144735d34749bc307
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-07 17:11:40 +0530

    Added Tests for Solution2 for Problem 6 in Python

--- Easy_Problems/6. Find Closest Value In BST/Python/Solution2.py
+++ Easy_Problems/6. Find Closest Value In BST/Python/Solution2.py
@@ -2,6 +2,8 @@
 # Worst: O(n) time | O(1) space
 # Iterative approach
 
+from BST import build_tree
+
 def findClosestValueInBst(tree, target):
  return findClosestValueInBstHelper(tree, target, float("inf"))
 
@@ -17,4 +19,14 @@ def findClosestValueInBstHelper(tree, target, closest):
             currentNode = currentNode.right
         else:
             break
-        return closest
+
+    return closest
+
+# Tests for findClosestValueInBst
+tree = build_tree([10, 4, 1, 6, 20])
+assert(findClosestValueInBst(tree, -1) == 1)
+assert(findClosestValueInBst(tree, 4) == 4)
+
+tree = build_tree([1, -2, 5, 40, 5,- 6, 7, 8, 9, 10])
+assert(findClosestValueInBst(tree, 25) == 40)
+assert(findClosestValueInBst(tree, -3) == -2)
