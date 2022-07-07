12b0b356a1fad81cb23b078c6e88651e937cae12
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-07 17:25:17 +0530

    Added BST Immplementation for Problem 6 in Javascript

---
+++ Easy_Problems/6. Find Closest Value In BST/Javascript/BST.js
@@ -0,0 +1,37 @@
+class Node
+{
+    constructor(data)
+    {
+        this.value = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        // root of a binary search tree
+        this.root = null;
+    }
+
+    insert(data) {
+        // Creating a node and initialising
+        // with data
+        var newNode = new Node(data);
+
+        // root is null then node will
+        // be added to the tree and made root.
+        if (this.root === null) this.root = newNode;
+        // find the correct position in the
+        // tree and add the node
+        else this.insertNode(this.root, newNode);
+    }
+
+    buildTree(arr) {
+        for (lwt i = 0; i < arr.length; i++) {
+            this.insert(arr[i]);
+        }
+    }
+}
+
+
