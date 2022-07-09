318465a1cb8c6573880cd7c954eb5edc071617cd
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-09 09:18:58 +0530

    Added Solution for Problem 7 in Python

---
+++ Easy_Problems/7. Branch Sums/Python/Solution.py
@@ -0,0 +1,23 @@
+# O(n) time | O(log(n)) space - where n is the number of nodes in the Binary Tree 
+# Worst: O(n) time | O(n) space - where n is the number of nodes in the Binary Tree 
+
+from BST import build_tree
+
+def getBranchSums(root):
+    sums = []
+    calculateBranchSums(root, 0, sums)
+    return sums
+
+def calculateBranchSums(node, runningSum, sums):
+    if node is None:
+        return
+
+    newRunningSum = runningSum + node.value
+    
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+    
+    calculateBranchSums(node.left, newRunningSum, sums)
+    calculateBranchSums(node.right, newRunningSum, sums)
+
