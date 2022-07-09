b86a1cb24ea109f4385896a9ea4c27f6c5dec434
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-09 10:14:17 +0530

    Added Solution 1 for Problem 8 in Python

---
+++ Easy_Problems/8. Node Depths/Python/Solution1.py
@@ -0,0 +1,15 @@
+# Average case: when the tree is balanced
+# O(n) time | O(h) space - where n is the number of nodes and h is the height of the Binary Tree
+
+def calculateNodeDepths(root):
+    sumOfDepths = 0
+    stack = [{"node": root, "depth": 0}]
+    while len(stack) > 0:
+        nodeInfo = stack.pop()
+        node, depth = nodeInfo["node"], nodeInfo["depth"]
+        if node is None:
+            continue
+        sumOfDepths += depth
+        stack.append({"node": node.left, "depth": depth + 1})
+        stack.append({"node": node.right, "depth": depth + 1})
+    return sumOfDepths
