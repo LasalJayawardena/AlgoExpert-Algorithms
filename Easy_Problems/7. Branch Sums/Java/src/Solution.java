2aad2e91e3a91b528b0044e5c50e38fa97f7dad6
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-09 09:45:26 +0530

    Added Tests for Solution for Problem 7 in Java

--- Easy_Problems/7. Branch Sums/Java/src/Solution.java
+++ Easy_Problems/7. Branch Sums/Java/src/Solution.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Solution {
     // O(n) time | O(n) space - where n is the number of nodes in the Bi
-    public static List<Integer> branchSums(BST tree) {
+    public static List<Integer> getBranchSums(BST tree) {
         List<Integer> sums = new ArrayList<>();
         calculateBranchSums(tree.root, 0, sums);
         return sums;
@@ -12,9 +12,20 @@ public class Solution {
     public static void calculateBranchSums(Node node, int runningSum, List<Integer> sums) {
         if (node == null) return;
         int newRunningSum = runningSum + node.value;
-        if (node.left == null && node.right == null) {
+
+        if ((node.left == null) && (node.right == null)) {
             sums.add(newRunningSum);
             return;
         }
+        
+        calculateBranchSums(node.left, newRunningSum, sums);
+        calculateBranchSums(node.right, newRunningSum, sums);
+    }
+
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.build(new int[]{1, 3, -1, 7, 2});
+
+        System.out.println(getBranchSums(bst)); // [0, 6, 11]
     }
 }
