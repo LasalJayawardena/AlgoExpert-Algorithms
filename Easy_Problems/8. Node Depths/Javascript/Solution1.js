5cda84a6047b63ead5511ff9c0c21df7a14149a2
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-09 10:30:24 +0530

    Added Solution 1 for Problem 8 in Java

---
+++ Easy_Problems/8. Node Depths/Javascript/Solution1.js
@@ -0,0 +1,16 @@
+// Average case: when the tree is balanced
+// O(n) time | O(h) space - where n is the number of nodes and h is the height of the Binary Tree
+
+
+function nodeDepths(root) {
+    let sumOfDepths = 0;
+    const stack = [{ node: root, depth: 0 }];
+    while (stack.length > 0) {
+        const { node, depth } = stack.pop();
+        if (node === null) continue;
+        sumOfDepths += depth;
+        stack.push({ node: node.left, depth: depth + 1 });
+        stack.push({ node: node.right, depth: depth + 1 });
+    }
+    return;
+}
