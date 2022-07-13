aaaa781f6cc370e26c6e03e5cf04737fd4ba61f4
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-13 23:15:02 +0530

    Added Solution 2 for Problem 10 in Javascript

---
+++ Easy_Problems/10. Minimum Waiting Time/Javascript/Solution2.js
@@ -0,0 +1,20 @@
+// Greedy Approach + Improved Logic
+// Intuition is that all remaining queries are waiting for the current query to finish, therefore need to add n times where n is the number of queries remaining
+// Time Complexity: O(N · log(N)), where N is the number of queries
+// Space Complexity: O(1)
+
+
+function getTotalMinWaitingTime(queries) {
+    queries.sort((a, b) => a - b);
+  
+    let totalWaitingTime = 0;
+  
+    for (let i = 0; i < queries.length; i++) {
+      const duration = queries[i];
+      const queriesLeft = queries.length - (i + 1);
+      totalWaitingTime += duration * queriesLeft;
+    }
+  
+    return totalWaitingTime;
+}
+
