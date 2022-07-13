6c0f08c9ddeb47ba416c1c77d3b193136f30c24f
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2022-07-13 23:20:55 +0530

    Added Tests for Solution 1 for Problem 10 in Java

--- Easy_Problems/10. Minimum Waiting Time/Java/src/Solution1.java
+++ Easy_Problems/10. Minimum Waiting Time/Java/src/Solution1.java
@@ -15,4 +15,14 @@ public class Solution1 {
 
         return totalWaitingTime;
     }
+
+    public static void main(String[] args) {
+        // Tests for getTotalMinWaitingTime
+        int[] queries = new int[] {3, 2, 1, 2, 6};
+        System.out.println(getTotalMinWaitingTime(queries)); // 17
+        queries = new int[] {10};
+        System.out.println(getTotalMinWaitingTime(queries)); // 0
+        queries = new int[] {3, 1};
+        System.out.println(getTotalMinWaitingTime(queries)); // 1
+    }
 }
